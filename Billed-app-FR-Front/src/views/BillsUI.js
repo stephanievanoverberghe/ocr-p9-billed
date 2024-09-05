815a5f3fdac3abfc1a351b079cd13cb36306b640
Stéphanie Vanoverberghe <orangestreet59123@gmail.com>
2024-09-05 18:36:58 +0200

    debug: login admin

--- Billed-app-FR-Front/src/views/BillsUI.js
+++ Billed-app-FR-Front/src/views/BillsUI.js
@@ -17,14 +17,20 @@ const row = (bill) => {
       </td>
     </tr>
     `)
-  }
-
-const rows = (data) => {
-  return (data && data.length) ? data.map(bill => row(bill)).join("") : ""
 }
 
+const rows = (data) => {
+  if (data && data.length) {
+    // Trier les factures par date décroissante
+    data.sort((a, b) => new Date(b.date) - new Date(a.date))
+    return data.map(bill => row(bill)).join("")
+  }
+  return ""
+}
+
+
 export default ({ data: bills, loading, error }) => {
-  
+
   const modal = () => (`
     <div class="modal fade" id="modaleFile" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
       <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
@@ -47,7 +53,7 @@ export default ({ data: bills, loading, error }) => {
   } else if (error) {
     return ErrorPage(error)
   }
-  
+
   return (`
     <div class='layout'>
       ${VerticalLayout(120)}
